3626f3bc3373eb006419ad3f32992d6e1a36a1a4
Simón Reyes Muñoz <nicolas.ra3333gmail.com>
2022-11-07 00:41:57 -0300

    intento fibonacci 1

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,2 +1,26 @@
+function fibonacci(limite) {
+
+    const arr_fib = [0, 1];
+    for(let i = 2; i < limite; i++) {
+            arr_fib[i] = arr_fib[i-1] + arr_fib[i-2];
+    }
+
+    return arr_fib;
+}
+
+const serie_fibonacci = fibonacci(1000);
+
+console.log(serie_fibonacci);
+
+let array_pares = serie_fibonacci.filter(function(numeros) {
+    return numeros % 2 == 0;
+})
+
+console.log(array_pares);
 
 
+let array_impares = serie_fibonacci.filter(function(numeros) {
+    return numeros % 3 == 0;
+})
+
+console.log(array_impares);
