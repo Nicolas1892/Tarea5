16d55b9d959b904adea43f4b3e36bbae624a8c42
Simón Reyes Muñoz <nicolas.ra3333gmail.com>
2022-11-06 23:25:50 -0300

    primer commit

---
+++ pokemon-M.js
@@ -0,0 +1,13 @@
+let pokemones =
+[
+ 'Pikachu',
+ 'Charmander',
+ 'Bulbasaur',
+ 'Squirtle'
+]
+
+const Poke_M = pokemones.map(element => {
+    return element.toUpperCase();
+});
+console.log(Poke_M);
+
---
+++ pokemon-fuego.js
@@ -0,0 +1,37 @@
+let pokemones = [
+ {
+ nombre: 'Pikachu',
+ tipo: 'Electrico'
+ },
+ {
+ nombre: 'Charmander',
+ tipo: 'Fuego',
+ },
+ {
+ nombre: 'Bulbasaur',
+ tipo: 'Planta'
+ },
+ {
+ nombre: 'Squirtle',
+ tipo: 'Agua'
+ },
+ {
+ nombre: 'Charmeleon',
+ tipo: 'Fuego'
+ },
+ {
+ nombre: 'Weedle',
+ tipo: 'bicho'
+ },
+ {
+ nombre: 'Charizard',
+ tipo: 'Fuego'
+ }
+]
+
+PokemonesFuego = [
+     pokemones[1], pokemones [4], pokemones[6]
+]
+
+console.log(PokemonesFuego);
+
